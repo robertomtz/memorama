2a7ec0e3ebe6fe6665f0fd3626a3eda05d1b1b92
Roberto Mtz <roberto@robertomtz.com>
2015-02-08 18:19:49 -0600

    Initial Commit

---
+++ Memorama/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Memorama
+//
+//  Created by Roberto Mtz on 2/8/15.
+//  Copyright (c) 2015 ITESM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
